229c414e89e5189a4fff83e37ce30655ebb8995f
jundymek <jundymek@gmail.com>
2020-02-18 23:01:47 +0100

    Updates

---
+++ precache-manifest.5b45e55b5018a0832260a4e66b73185d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81d6baf53825774fdb2180252c7cbf6",
+    "url": "/chat-messenger/index.html"
+  },
+  {
+    "revision": "8887f49116fcdc366d9d",
+    "url": "/chat-messenger/static/css/main.b88ec80d.chunk.css"
+  },
+  {
+    "revision": "e14778502cf9ee0cb0bd",
+    "url": "/chat-messenger/static/js/2.740c53f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/chat-messenger/static/js/2.740c53f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8887f49116fcdc366d9d",
+    "url": "/chat-messenger/static/js/main.d326df17.chunk.js"
+  },
+  {
+    "revision": "6a1c889081875aa81318",
+    "url": "/chat-messenger/static/js/runtime-main.7c14cccd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chat-bot-typescript/precache-manifest.0c90729f286a69021b46efa1ed96bd4d.js"
+  "/chat-messenger/precache-manifest.5b45e55b5018a0832260a4e66b73185d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chat-bot-typescript/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chat-messenger/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
