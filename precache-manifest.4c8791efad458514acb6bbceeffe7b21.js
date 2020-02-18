96fa0212f39c01acda1e9bdd1ed1635a4f76b020
jundymek <jundymek@gmail.com>
2020-02-18 01:23:10 +0100

    Updates

---
+++ precache-manifest.4c8791efad458514acb6bbceeffe7b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6aebe19d32535921190dca87214f43",
+    "url": "/chat-bot-typescript/index.html"
+  },
+  {
+    "revision": "4ea911bb43f0b6188e6c",
+    "url": "/chat-bot-typescript/static/css/main.b8145f66.chunk.css"
+  },
+  {
+    "revision": "e14778502cf9ee0cb0bd",
+    "url": "/chat-bot-typescript/static/js/2.740c53f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/chat-bot-typescript/static/js/2.740c53f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea911bb43f0b6188e6c",
+    "url": "/chat-bot-typescript/static/js/main.9399f83e.chunk.js"
+  },
+  {
+    "revision": "e5f63025980f09d47a5b",
+    "url": "/chat-bot-typescript/static/js/runtime-main.54005e9f.js"
+  }
+]);
